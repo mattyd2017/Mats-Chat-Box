3b659a1160803d08abdcd2b523e7b01adef58223
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2018-09-25 18:17:50 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var http = require("http");
+
+var app = express();
+var server = http.Server(app);
+var io = require("socket.io")(server);
+var users = [];
+
+server.listen(3332, function(){
+    console.log("Server address is port 3332.");
+});
+
+app.get("/",function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+app.get("/styles/index.css",function(req, res){
+    res.sendFile(__dirname + "/styles/index.css");
+});
+io.on("connection", function(socket){
+   var name ="";
+    socket.on("has connected", function(username){
+           name = username;
+           users.push(username); 
+           io.emit("has connected",{username: username, userList: users});
+    });
+
+    socket.on("disconnect", function(){
+       users.splice(users.indexOf(name), 1); 
+       io.emit("has disconnected",{username: name, userList: users});
+    });
+    socket.on("new message", function(data){
+        io.emit("new message", data);
+    });
+});
